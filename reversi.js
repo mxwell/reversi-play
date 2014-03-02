c165d92d664fba8952a992e74b4a23309aa6a2c8
Murat Khayrulin <murat.khairulin@gmail.com>
2014-03-02 14:15:28 +0400

    Replace canvas to SVG completely.

--- reversi.js
+++ reversi.js
@@ -1,5 +1,5 @@
 /*
-  Cell entity:
+  Disk entity:
   - x : Integer
   - y : Integer
   - side : Integer (1 or 2)
@@ -7,22 +7,25 @@
 
 Players = new Meteor.Collection("players");
 Disks = new Meteor.Collection("disks");
-var CELL_SIZE = 33; /* including border at one side */
-var CELL_BORDER = 3;
-var CELL_CENTER_OFFSET = CELL_BORDER + CELL_SIZE / 2;
-var DISK_RADIUS = CELL_SIZE * 0.375;
-var FLIP_INC = DISK_RADIUS / 5;
-var FLIP_DELAY = 100; /* ms */
-var DISK_BORDER = 2;
-var N = 8;
-var GRID_BORDER = 0;
-var GRID_SIZE = (CELL_SIZE) * N + CELL_BORDER + 2 * GRID_BORDER;
-var BG_COLOR = '#00CC66'; /* nice green */
-var GRID_COLOR = '#003366'; /* dark blue */
-var DISK_BORDER_COLOR = '#002E5C'; /* medium blue */
-var DISK_DARK_SIDE  = '#000000'; /* dark */
-var DISK_LIGHT_SIDE = '#FFFFFF'; /* bright */
-var VACANT_COLOR = '#00FF00'; /* real green */
+var CELL_SIZE           = 33; /* including border at one side */
+var CELL_BORDER         = 2;
+var CELL_CENTER_OFFSET  = CELL_BORDER + CELL_SIZE / 2;
+var DISK_RADIUS         = CELL_SIZE * 0.375;
+var DISK_VACANT_RADIUS  = DISK_RADIUS / 5;
+var FLIP_INC            = DISK_RADIUS / 5;
+var FLIP_ANIMATE_TIME   = 500; /* ms */
+var FLIP_ANIMATE_STEPS  = 10;
+var FLIP_DELAY          = FLIP_ANIMATE_TIME / FLIP_ANIMATE_STEPS;
+var DISK_BORDER         = 2;
+var N                   = 8;
+var GRID_BORDER         = 4;
+var GRID_SIZE           = (CELL_SIZE) * N + CELL_BORDER + 2 * GRID_BORDER;
+var BG_COLOR            = '#00CC66'; /* nice green */
+var GRID_COLOR          = '#003366'; /* dark blue */
+var DISK_BORDER_COLOR   = '#002E5C'; /* medium blue */
+var DISK_DARK_SIDE      = '#000000'; /* dark */
+var DISK_LIGHT_SIDE     = '#FFFFFF'; /* bright */
+var VACANT_COLOR        = '#00FF00'; /* real green */
 var adj = [
     [-1, -1], [-1, 0], [-1, 1],
     [0, -1], [0, 1],
@@ -34,12 +37,11 @@ var VACANT_CELL = 3;
 if (Meteor.isClient) {
     var cells = new Array(N);
     var field = undefined;
-    var flip_context = undefined;
+    var flipContext = undefined;
     for (var i = 0; i < N; ++i)
         cells[i] = new Array(N);
 
     var getField = function () {
-        console.log("typeof: " + typeof field);
         if (typeof field === 'undefined') {
             console.log("creating SVG-based field");
             var svg = d3.select("#grid").append("svg")
@@ -53,7 +55,7 @@ if (Meteor.isClient) {
                         .attr("fill", BG_COLOR);
             var borders = []
             var range = _.range(N + 1);
-            var borders = _.map(range, function(i) {
+            var borders = _.map(range, function (i) {
                 return GRID_BORDER + i * CELL_SIZE; 
             });
             var horizontalGroup = svg.append("g");
@@ -62,10 +64,12 @@ if (Meteor.isClient) {
                             .data(borders)
                             .enter()
                             .append("line");
+            /* rightmost or bottom point of line */
+            var greaterValue = GRID_BORDER + N * CELL_SIZE + CELL_BORDER / 2;
             var horizontalsAttrs = horizontals
                                     .attr("x1", GRID_BORDER)
                                     .attr("y1", function(d) { return d; })
-                                    .attr("x2", GRID_BORDER + N * CELL_SIZE)
+                                    .attr("x2", greaterValue)
                                     .attr("y2", function(d) { return d; })
                                     .attr("stroke-width", CELL_BORDER)
                                     .attr("stroke", GRID_COLOR);
@@ -77,12 +81,17 @@ if (Meteor.isClient) {
                                     .attr("x1", function(d) { return d; })
                                     .attr("y1", GRID_BORDER)
                                     .attr("x2", function(d) { return d; })
-                                    .attr("y2", GRID_BORDER + N * CELL_SIZE)
+                                    .attr("y2", greaterValue)
                                     .attr("stroke-width", CELL_BORDER)
                                     .attr("stroke", GRID_COLOR);
             var darkGroup = svg.append("g").attr("class", "dark");
             var lightGroup = svg.append("g").attr("class", "light");
+            var flipGroup = svg.append("g").attr("class", "flip");
             var vacantGroup = svg.append("g").attr("class", "vacant");
+            var flipRadii = _.map(_.range(FLIP_ANIMATE_STEPS + 1), function (i) {
+                var angle = Math.PI * i / FLIP_ANIMATE_STEPS;
+                return DISK_RADIUS * Math.abs(Math.cos(angle));
+            });
             field = {
                 "svg": svg,
                 "bg": bg,
@@ -90,7 +99,9 @@ if (Meteor.isClient) {
                 "verticals": verticals,
                 "dark": darkGroup,
                 "light": lightGroup,
-                "vacant": vacantGroup
+                "flip": flipGroup,
+                "vacant": vacantGroup,
+                "radii": flipRadii
             };
         }
         return field;
@@ -104,23 +115,15 @@ if (Meteor.isClient) {
         return GRID_BORDER + cellId * CELL_SIZE + CELL_CENTER_OFFSET - 2;
     }
 
-    var clearFlipped = function () {
-        var flipped = _.map(Disks.find({flip: true}).fetch(), function(disk) {
-            return disk._id;
-        });
-        console.log("flipped: " + flipped.length);
-        _.map(flipped, function(id) {
-            Disks.update({_id: id}, {$set: {flip: false}});
-        });
-    }
-
     var drawDisksOnSvg = function () {
-        console.log("drawDisksOnSvg called");
         var darkDisks = Disks.find({$and: [{side: 1}, {flip: {$not: true}}]});
         var lightDisks = Disks.find({$and: [{side: 2}, {flip: {$not: true}}]});
         var fd = getField();
         fd.dark.selectAll("circle").remove();
         fd.light.selectAll("circle").remove();
+        flipContext = undefined;
+        fd.flip.selectAll("ellipse").remove();
+        fd.vacant.selectAll("circle").remove();
         fd.dark
             .selectAll("circle")
             .data(darkDisks.fetch())
@@ -143,32 +146,28 @@ if (Meteor.isClient) {
             .style("fill", DISK_LIGHT_SIDE)
             .style("stroke", DISK_BORDER_COLOR)
             .style("stroke-width", DISK_BORDER);
-        fd.vacant
-            .selectAll("circle")
-            .remove();
+        
         var flippingDisks = Disks.find({flip: true}).fetch();
         if (flippingDisks.length === 0) {
-            fd
-                .vacant
-                .selectAll("ellipse")
-                .remove();
+            drawVacancies();
             return;
         }
         var selection = fd
-            .vacant
+            .flip
             .selectAll("ellipse")
             .data(flippingDisks)
             .enter()
             .append("ellipse");
         var startSide = 3 - flippingDisks[0].side;
-        drawEllipsis(selection, DISK_RADIUS, sideToColor(startSide));
-        flip_context = {
-            /* changes from -R to R */
-            "r": -DISK_RADIUS,
+        flipContext = {
+            /* changes from 0 to FLIP_ANIMATE_STEPS */
+            "step": 0,
+            "radii": fd.radii,
             "side": startSide,
-            "selection": selection
+            "selection": selection,
+            "active": true,
         };
-        setTimeout(flipHandler, FLIP_DELAY);
+        flipHandler();
     }
 
     var sideToColor = function(side) {
@@ -194,67 +193,95 @@ if (Meteor.isClient) {
         return Math.min(r, DISK_RADIUS);
     }
 
-    var flipHandler = function handler(ctx) {
-        var context = flip_context;
-        var prevR = context.r;
-        var nextR = modifyRadius(prevR);
-        if (prevR === nextR) {
+    var clearFlipped = function () {
+        var flipped = _.map(Disks.find({flip: true}).fetch(), function(disk) {
+            return disk._id;
+        });
+        _.map(flipped, function(id) {
+            Disks.update({_id: id}, {$set: {flip: false}});
+        });
+    }
+
+    var flipHandler = function handler() {
+        var context = flipContext;
+        if (typeof context === 'undefined')
+            return;
+        var step = context.step;
+        if (step >= FLIP_ANIMATE_STEPS) {
             clearFlipped();
+            flipContext.active = false;
             return;
         }
-        if (prevR < -1e-3 && (Math.abs(nextR) < 1e-3 || nextR > 0))
+        if (step == (FLIP_ANIMATE_STEPS - FLIP_ANIMATE_STEPS / 2))
             context.side = 3 - context.side;
-        context.r = nextR;
-        drawEllipsis(context.selection, Math.abs(context.r),
+        drawEllipsis(context.selection, context.radii[step],
             sideToColor(context.side));
+        context.step = step + 1;
         setTimeout(handler, FLIP_DELAY);
     }
 
-    var drawGrid = function() {
-        drawGridOnSvg();
-        var canv = $('#canv')[0];
-        var ctx = canv.getContext('2d');
-        if (canv.width !== GRID_SIZE || canv.height !== GRID_SIZE) {
-            console.log("resizing");
-            canv.width = GRID_SIZE;
-            canv.height = GRID_SIZE;
-            $('#canv').css("width", GRID_SIZE);
-            $('#canv').css("height", GRID_SIZE);
-            var half = Math.floor(GRID_SIZE / 2);
-            $('#first').css("width", half);
-            $('#second').css("width", half);
-            $('#status').css("width", 2 * half);
+    var findVacancies = function (player) {
+        var other = 3 - player;
+        var vacancies = []
+        for (var x = 0; x < N; ++x)
+            for (var y = 0; y < N; ++y)
+                if (cells[x][y] === player)
+                    for (var k = 0; k < adj.length; ++k) {
+                        var dx = adj[k][0],
+                            dy = adj[k][1];
+                        var u = x + dx;
+                        var v = y + dy;
+                        var len = 0;
+                        while (cellValid(u, v) && cells[u][v] === other) {
+                            u += dx;
+                            v += dy;
+                            ++len;
+                        }
+                        if (len > 0 && cellValid(u, v) && cells[u][v] === FREE_CELL) {
+                            cells[u][v] = VACANT_CELL;
+                            vacancies.push({"x": u, "y": v});
+                        }
+                    }
+        return vacancies;
+    };
+
+    var drawVacancies = function () {
+        if (!gameRunning())
+            return;
+        var player = Players.findOne({active: true});
+        /* game is not running? */
+        if (typeof player === 'undefined')
+            return;
+        var player = player.id;
+        console.log("player found: " + player);
+        var vacancies = findVacancies(player);
+        console.log("found vacancies: " + vacancies.length);
+        if (vacancies.length === 0) {
+            player = nextMove();
+            console.log("trying with another player: " + player);
+            if (typeof player === 'undefined')
+                return;
+            vacancies = findVacancies(player);
+            if (vacancies.length === 0) {
+                console.log("vacancies not found for both players");
+                finishGame();
+            }
         }
-        ctx.fillStyle = BG_COLOR;
-        ctx.fillRect(0, 0, canv.width, canv.height);
-        var i;
-        ctx.fillStyle = GRID_COLOR;
-        for (i = 0; i <= N; ++i) {
-            var start = i * CELL_SIZE;
-            var end = start + CELL_BORDER;
-            ctx.fillRect(0, i * CELL_SIZE, canv.width, CELL_BORDER);
-            ctx.fillRect(i * CELL_SIZE, 0, CELL_BORDER, canv.height);
-        }
+        var fd = getField();
+        fd.vacant
+            .selectAll("circle")
+            .data(vacancies)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) { return cellIdToCoordinate(d.x); })
+            .attr("cy", function (d) { return cellIdToCoordinate(d.y); })
+            .attr("r", DISK_VACANT_RADIUS)
+            .style("fill", VACANT_COLOR)
+            .style("stroke", DISK_BORDER_COLOR)
+            .style("stroke-width", DISK_BORDER);
     }
 
-    var drawDisk = function (xId, yId, color, r) {
-        //console.log("drawDisk");
-        var canv = $('#canv')[0];
-        var ctx = canv.getContext('2d');
-        var centerX = xId * CELL_SIZE + CELL_CENTER_OFFSET;
-        var centerY = yId * CELL_SIZE + CELL_CENTER_OFFSET;
-        ctx.beginPath();
-        if (typeof r === 'undefined')
-            r = DISK_RADIUS;
-        ctx.arc(centerX, centerY, r, 0, 2 * Math.PI, false);
-        ctx.fillStyle = color;
-        ctx.fill();
-        ctx.lineWidth = DISK_BORDER;
-        ctx.strokeStyle = DISK_BORDER_COLOR;
-        ctx.stroke();
-    }
-
-    var cell_is_valid = function (i, j) {
+    var cellValid = function (i, j) {
         return 0 <= i && i < N && 0 <= j && j < N;
     }
 
@@ -272,12 +299,12 @@ if (Meteor.isClient) {
             var u = x + dx;
             var v = y + dy;
             var len = 0;
-            while (cell_is_valid(u, v) && cells[u][v] === other) {
+            while (cellValid(u, v) && cells[u][v] === other) {
                 u += dx;
                 v += dy;
                 ++len;
             }
-            if (len > 0 && cell_is_valid(u, v) && cells[u][v] === player) {
+            if (len > 0 && cellValid(u, v) && cells[u][v] === player) {
                 for (; len > 0; --len) {
                     u -= dx;
                     v -= dy;
@@ -286,56 +313,10 @@ if (Meteor.isClient) {
                 }
             }
         }
-        next_move();
+        nextMove();
     }
 
-    var mark_as_vacant = function (i, j) {
-        //console.log("Cell [" + i + "," + j + "] is vacant");
-        cells[i][j] = VACANT_CELL;
-        drawDisk(i, j, VACANT_COLOR, DISK_RADIUS / 5);
-    }
-
-    var try_find_vacant = function (player) {
-        var other = 3 - player;
-        var cnt = 0;
-        for (var x = 0; x < N; ++x)
-            for (var y = 0; y < N; ++y)
-                if (cells[x][y] === player)
-                    for (var k = 0; k < adj.length; ++k) {
-                        var dx = adj[k][0],
-                            dy = adj[k][1];
-                        var u = x + dx;
-                        var v = y + dy;
-                        var len = 0;
-                        while (cell_is_valid(u, v) && cells[u][v] === other) {
-                            u += dx;
-                            v += dy;
-                            ++len;
-                        }
-                        if (len > 0 && cell_is_valid(u, v) && cells[u][v] === FREE_CELL) {
-                            mark_as_vacant(u, v);
-                            ++cnt;
-                        }
-                    }
-        return cnt;
-    };
-
-    /* make sure game is running before call */
-    var find_vacant = function() {
-        //console.log("find_vacant called");
-        var player = Players.findOne({active: true}).id;
-        if (try_find_vacant(player) === 0) {
-            player = next_move();
-            if (typeof player === 'undefined')
-                return;
-            if (try_find_vacant(player) === 0) {
-                console.log("vacant places not found for both players");
-                finish_game();
-            }
-        }
-    }
-
-    var game_is_running = function () {
+    var gameRunning = function () {
         return Players.find({active: true}).count() === 1 &&
                Players.find({active: false}).count() === 1 &&
                 Disks.find({}).count() >= 4;
@@ -348,6 +329,7 @@ if (Meteor.isClient) {
         var players = Players.find({}).count();
         for (; players > 0; --players)
             Players.remove({_id: Players.findOne({})._id});
+        flipContext = undefined;
         var rg = Math.floor(N / 2);
         var lf = rg - 1;
         Disks.insert({x: lf, y: lf, side: 1});
@@ -358,7 +340,7 @@ if (Meteor.isClient) {
         Players.insert({id: 2, active: false});
     }
 
-    var next_move = function () {
+    var nextMove = function () {
         var active = Players.findOne({active: true});
         var inactive = Players.findOne({active: false});
         if (typeof active !== 'undefined' && typeof inactive !== 'undefined') {
@@ -369,23 +351,33 @@ if (Meteor.isClient) {
         return undefined;
     }
 
-    var finish_game = function () {
+    var finishGame = function () {
         var active = Players.findOne({active: true});
         var inactive = Players.findOne({active: false});
         if (typeof active !== 'undefined' && typeof inactive !== 'undefined')
             Players.update({_id: active._id}, {$set: {active: false}});
     }
 
+    var currentPlayer = function () {
+        return Players.findOne({active: true}).id;
+    }
+
     Template.board.events({
-        'click #canv' : (function (event) {
-            console.log("Canvas clicked");
-            if (!game_is_running())
+        'click svg' : (function (event) {
+            console.log("svg clicked");
+            if (typeof flipContext !== 'undefined' && flipContext.active) {
+                console.log("ignored because animation");
                 return;
-            var canv = $('#canv')[0];
-            var canvOffsetX = canv.offsetLeft;
-            var canvOffsetY = canv.offsetTop;
-            var x = event.pageX - canvOffsetX,
-                y = event.pageY - canvOffsetY;
+            }
+            var svg = $("#grid svg")[0];
+            var bRect = svg.getBoundingClientRect();
+            var x = event.pageX - bRect.left - GRID_BORDER;
+            var y = event.pageY - bRect.top - GRID_BORDER;
+            console.log(" in (" + x + ", " + y + ")");
+            if (x <= 0 || y <= 0) {
+                console.log("miss: top or left border");
+                return;
+            }
             if (x % CELL_SIZE < CELL_BORDER || y % CELL_SIZE < CELL_BORDER) {
                 console.log("miss: border");
                 return;
@@ -394,10 +386,9 @@ if (Meteor.isClient) {
             var yId = Math.floor(y / CELL_SIZE);
             if (xId >= N || yId >= N) {
                 console.log("miss: rightmost or bottom border");
-                return;
             }
             console.log("hit cell " + xId + "," + yId);
-            addDisk(xId, yId, Players.findOne({active: true}).id);
+            addDisk(xId, yId, currentPlayer());
         }),
         'click #reset_button' : (function (event) {
             console.log("Reset button clicked");
@@ -409,19 +400,14 @@ if (Meteor.isClient) {
         var self = this;
         if (!self.handle) {
             self.handle = Deps.autorun(function () {
-                drawGrid();
                 var disks = Disks.find();
                 for (var i = 0; i < N; ++i)
                     for (var j = 0; j < N; ++j)
                         cells[i][j] = FREE_CELL;
                 disks.forEach(function(disk) {
-                    drawDisk(disk.x, disk.y,
-                        disk.side === 1 ? DISK_DARK_SIDE : DISK_LIGHT_SIDE);
                     cells[disk.x][disk.y] = disk.side;
                 });
                 drawDisksOnSvg();
-                if (game_is_running())
-                    find_vacant();
             });
         }
     };
